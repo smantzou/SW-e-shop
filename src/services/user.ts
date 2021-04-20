439a48871ee0d26e775797aaff9a41a3a4984955
Stelios Mantzouranis <steliosmantzouranis@gmail.com>
2021-04-20 20:59:03 +0300

    Git author test

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import WrongCredentialsException from '../exceptions/WrongCredentialsException';
 import * as express from 'express';
